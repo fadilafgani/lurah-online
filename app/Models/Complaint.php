7cb2c59b73138c7ac3da6977d196ac3d52e0ebb7
Sheilaa5 <shintaaulya20@gmail.com>
2026-06-05 07:04:07 +0700

    Add complaint API endpoint and model updates

---
+++ app/Models/Complaint.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Complaint extends Model
+{
+    protected $fillable = [
+        'ticket_code',
+        'name',
+        'phone',
+        'email',
+        'title',
+        'description',
+        'location',
+        'category',
+        'photo',
+        'status',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\ComplaintController;
 
 Route::post('/admin/login', [AuthController::class, 'login']);
+
+Route::post('/complaints', [ComplaintController::class, 'store']);
+Route::get('/complaints/{ticket_code}', [ComplaintController::class, 'show']);
+Route::get('/statistics', [ComplaintController::class, 'statistics']);
